cd121f11e2a9f5e6ee17e95b4d5f8cecd97099f6
fernandes-draw <fernandes_draw@hotmail.com>
2025-04-26 13:05:49 -0300

    episode-02

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-const heading = React.createElement(
-  'h1',
-  { id: 'heading' },
-  'Hello world from React'
-);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, 'This is Namaste React'),
+    React.createElement('h2', {}, 'by Everson Fernandes'),
+  ]),
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement('h1', {}, "I'm an h1 tag"),
+    React.createElement('h2', {}, "I'm an h2 tag"),
+  ]),
+]);
+
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+
+root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React Course
+
+## Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algoritm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and production bundles
